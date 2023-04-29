1e1ac5f8488c5116214c1e8888d221697e6c180c
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-04-29 23:53:20 +0600

    add tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-
-import styles from '@/styles/Home.module.css'
-
-
 
 export default function Home() {
   return (
    <div>
-    <h1>Hello world</h1>
+    <h1 className="font-bold">Hello world</h1>
    </div>
   )
 }

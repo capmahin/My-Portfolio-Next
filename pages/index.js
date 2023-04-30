4e93954e418931187fcf0e6229f6df6acc1f6d51
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-05-01 00:28:48 +0600

    add portfolio

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import HomePage from "../components/HomePage";
+import Portfolio from "../components/Portfolio";
 export default function Home() {
   return (
    <div>
     <HomePage />
+    <Portfolio />
    </div>
   )
 }

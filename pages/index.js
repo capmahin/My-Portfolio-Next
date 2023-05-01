3613c8e0ec23d7415ac07aaedfb1edac79db67b2
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-05-01 17:47:10 +0600

    add experience in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import HomePage from "../components/HomePage";
 import Portfolio from "../components/Portfolio";
+import Experience from "../components/Experience";
 export default function Home() {
   return (
    <div>
     <HomePage />
     <Portfolio />
+    <Experience />
    </div>
   )
 }
